901af8167e89631ff2b2e9020eb51ab0df4fd759
basavaraj <basavarajdarur7@gmail.com>
2023-11-24 19:26:17 +0530

    react project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <marquee>Welcome To RBASAVARAJ </marquee>
+        <p>
+         Hi Hello Welcome to RBASAVARAJ
+        </p>
+      
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
